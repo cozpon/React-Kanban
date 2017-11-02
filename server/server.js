c1f4d2cba9f17602287cc4142d87a80049f2ed21
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-11-02 10:59:20 -1000

    Database GET working

--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
 const express = require('express')
 const app = express()
+const router = express.Router();
 const bodyParser = require('body-parser');
 const db = require('../models');
 const PORT = process.env.PORT || 4567;
 
-const users = db.users;
-const cards = db.cards;
+const Users = db.users;
+const Cards = db.cards;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ "extended" : true }));
 
+
 app.post('/api/users', (req, res) => {
-  const data = req.body;
-  return users.create({ username: data.username })
+  let data = req.body;
+  return Users.create({ username: data.username })
   .then(data => {
     res.json(data);
+  })
+})
+
+app.get('/api/users', (req, res) => {
+  return Users.findAll()
+  .then(users => {
+    res.json(users);
   })
 })
 
 
 app.post('/api/cards', (req, res) => {
   const data = req.body;
-  return cards.create({
+  return Cards.create({
     title: data.title,
     created_by: data.created_by,
     priorityId: data.priorityId,
@@ -32,6 +41,16 @@ app.post('/api/cards', (req, res) => {
     res.json(data);
   })
 })
+
+
+
+app.get('/api/cards', (req, res) => {
+  return Cards.findAll()
+  .then(cards => {
+    res.json(cards);
+  })
+})
+
 
 app.listen(PORT, function() {
   db.sequelize.sync({ force: false });
